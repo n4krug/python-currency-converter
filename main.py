bb451b4f530c877bc44073b2ad5013d0d8d27743
n4krug <gustav.eneberg@gmail.com>
2021-09-29 17:24:12 +0200

    updated main.py

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import requests
 import json
 import sys
 
+OE_APP_ID = ""
 
 # Global DEFs
-with open('OE_APP_ID', 'r')as f:
+with open("OE_APP_ID", "r") as f:
     OE_APP_ID = f.read()
 
 
 # Get latest currency data from openexchangerates
 def get_rates(base):
-    
-    params = {
-        "app_id": OE_APP_ID,
-        "base": base
-    }
 
-    currencies = requests.get(f"https://openexchangerates.org/api/latest.json", params).json()
+    params = {"app_id": OE_APP_ID, "base": base}
+
+    currencies = requests.get(
+        f"https://openexchangerates.org/api/latest.json", params
+    ).json()
 
     rates = currencies["rates"]
 
@@ -43,8 +43,20 @@ def main():
 
     base = args[0]
 
-    in_amount = args[1]
+    in_amount = int(args[1])
 
     out_currency = args[2]
 
-    rates = get_rates(base)
+    rates = get_rates("USD")
+
+    amount_USD = round(in_amount / rates[base], 2)
+
+    amount_out = round(amount_USD * rates[out_currency], 2)
+
+    print(f"{in_amount} {base}")
+    print(f"{amount_USD} USD")
+    print(f"{amount_out} {out_currency}")
+
+
+if __name__ == "__main__":
+    main()
