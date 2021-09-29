b683c6b5a164d37d0e0321c916e63fed1818dea6
n4krug <gustav.eneberg@gmail.com>
2021-09-29 21:07:58 +0200

    Made Currency conversion a Class

--- main.py
+++ main.py
@@ -2,25 +2,39 @@ import requests
 import json
 import sys
 
+
+class CurrencyConverter:
+    def __init__(self, app_id):
+        self.app_id = app_id
+
+    # Get latest currency data from openexchangerates
+    def get_rates(self, base):
+
+        params = {"app_id": self.app_id, "base": base}
+
+        currencies = requests.get(
+            f"https://openexchangerates.org/api/latest.json", params
+        ).json()
+
+        rates = currencies["rates"]
+
+        return rates
+
+    def convert_currencies(
+        self, rates, in_amount, in_currency="USD", out_currency="EUR"
+    ):
+        USD_amount = round(in_amount / rates[in_currency], 2)
+
+        out_amount = round(USD_amount * rates[out_currency], 2)
+
+        return out_amount
+
+
 OE_APP_ID = ""
 
 # Global DEFs
 with open("OE_APP_ID", "r") as f:
     OE_APP_ID = f.read()
-
-
-# Get latest currency data from openexchangerates
-def get_rates(base):
-
-    params = {"app_id": OE_APP_ID, "base": base}
-
-    currencies = requests.get(
-        f"https://openexchangerates.org/api/latest.json", params
-    ).json()
-
-    rates = currencies["rates"]
-
-    return rates
 
 
 def help():
@@ -39,6 +53,8 @@ def get_args():
 
 def main():
 
+    c_converter = CurrencyConverter(OE_APP_ID)
+
     args = get_args()
 
     base = args[0]
@@ -47,14 +63,11 @@ def main():
 
     out_currency = args[2]
 
-    rates = get_rates("USD")
+    rates = c_converter.get_rates("USD")
 
-    amount_USD = round(in_amount / rates[base], 2)
-
-    amount_out = round(amount_USD * rates[out_currency], 2)
+    amount_out = c_converter.convert_currencies(rates, in_amount, base, out_currency)
 
     print(f"{in_amount} {base}")
-    print(f"{amount_USD} USD")
     print(f"{amount_out} {out_currency}")
 
 
